9c1356d870d61e7d28a6cf1536d1c8d22d002fd2
Matt Benjamin <matt@linuxbox.com>
2013-12-23 13:19:40 -0500

    Header and spacing cleanups (simple_dispatcher).
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/test/messenger/simple_dispatcher.cc
+++ src/test/messenger/simple_dispatcher.cc
@@ -1,61 +1,69 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
 /*
- * SimpleDispatcher.cpp
+ * Ceph - scalable distributed file system
  *
- *  Created on: Nov 21, 2013
- *      Author: matt
+ * Copyright (C) 2013 CohortFS, LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
  */
 
 #include "simple_dispatcher.h"
 #include "messages/MPing.h"
 
 SimpleDispatcher::SimpleDispatcher(Messenger *msgr) :
-	Dispatcher(msgr->cct),
-	active(false),
-	messenger(msgr) {
-
+  Dispatcher(msgr->cct),
+  active(false),
+  messenger(msgr)
+{
+  // nothing
 }
 
 SimpleDispatcher::~SimpleDispatcher() {
-	// nothing
+  // nothing
 }
 
 bool SimpleDispatcher::ms_dispatch(Message *m)
 {
-	ConnectionRef conn;
-	int code;
+  ConnectionRef conn;
+  int code;
 
-	cout << __func__ << " " << m << std::endl;
+  cout << __func__ << " " << m << std::endl;
 
-	switch (m->get_type()) {
-	case CEPH_MSG_PING:
-		if (active) {
-			cout << "pong!" << std::endl;
-		} else {
-			cout << "ping!" << std::endl;
-			conn = m->get_connection();
-			code = messenger->send_message(new MPing(), conn);
-			if (code != 0) {
-			    cout << "send_reply returned " << code << std::endl;
-			    return false;
-			}
-		}
-		/* XXXX the below put() works correctly with SimpleMessenger
-		 * but crashes with XioMessenger */
-		//m->put();
-		break;
-	default:
-		abort();
-	}
-	return true;
+  switch (m->get_type()) {
+  case CEPH_MSG_PING:
+    if (active) {
+      cout << "pong!" << std::endl;
+    } else {
+      cout << "ping!" << std::endl;
+      conn = m->get_connection();
+      code = messenger->send_message(new MPing(), conn);
+      if (code != 0) {
+	cout << "send_reply returned " << code << std::endl;
+	return false;
+      }
+    }
+    /* XXXX the below put() works correctly with SimpleMessenger
+     * but crashes with XioMessenger */
+    //m->put();
+    break;
+  default:
+    abort();
+  }
+  return true;
 }
 
 bool SimpleDispatcher::ms_handle_reset(Connection *con)
 {
-	return true;
+  return true;
 }
 
 void SimpleDispatcher::ms_handle_remote_reset(Connection *con)
 {
-	// nothing
+  // nothing
 }
 
