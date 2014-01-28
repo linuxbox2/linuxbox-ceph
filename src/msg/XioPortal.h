49e67b2881445962608cfa3af92a9ff0e768264d
Matt Benjamin <matt@linuxbox.com>
2014-01-28 18:42:21 -0500

    Multi-lane submit queue.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/msg/XioPortal.h
+++ src/msg/XioPortal.h
@@ -24,16 +24,79 @@ extern "C" {
 #include "XioMsg.h"
 #include <boost/lexical_cast.hpp>
 
+#ifndef CACHE_LINE_SIZE
+#define CACHE_LINE_SIZE 64 /* XXX arch-specific define */
+#endif
+#define CACHE_PAD(_n) char __pad ## _n [CACHE_LINE_SIZE]
+
 class XioMessenger;
 
 class XioPortal : public Thread
 {
 private:
+
+  struct SubmitQueue
+  {
+    const static int nlanes = 7;
+
+    struct Lane
+    {
+      uint32_t size;
+      XioMsg::Queue q;
+      pthread_spinlock_t sp;
+      CACHE_PAD(0);
+    };
+
+    Lane qlane[nlanes];
+
+    SubmitQueue()
+      {
+	int ix;
+	Lane* lane;
+
+	for (ix = 0; ix < nlanes; ++ix) {
+	  lane = &qlane[ix];
+	  pthread_spin_init(&lane->sp, PTHREAD_PROCESS_PRIVATE);
+	  lane->size = 0;
+	}
+      }
+
+    inline Lane* get_lane()
+      {
+	return &qlane[((uint64_t) pthread_self()) % nlanes];
+      }
+
+    void enq(XioMsg* xmsg)
+      {
+	Lane* lane = get_lane();
+	pthread_spin_lock(&lane->sp);
+	lane->q.push_back(*xmsg);
+	++(lane->size);
+	pthread_spin_unlock(&lane->sp);
+      }
+
+    void deq(XioMsg::Queue &send_q)
+      {
+	int ix;
+	Lane* lane;
+
+	for (ix = 0; ix < nlanes; ++ix) {
+	  lane = &qlane[ix];
+	  pthread_spin_lock(&lane->sp);
+	  if (lane->size > 0) {
+	    send_q.splice(send_q.end(), lane->q);
+	    lane->size = 0;
+	  }
+	  pthread_spin_unlock(&lane->sp);
+	}
+      }
+
+  };
+
   XioMessenger *msgr;
   struct xio_context *ctx;
   struct xio_server *server;
-  XioMsg::Queue submit_q;
-  uint32_t sq_size;
+  SubmitQueue submit_q;
   pthread_spinlock_t sp;
   void *ev_loop;
   string xio_uri;
@@ -47,12 +110,11 @@ private:
 
 public:
   XioPortal(XioMessenger *_msgr) :
-  msgr(_msgr), ctx(NULL), server(NULL), xio_uri(""),
+  msgr(_msgr), ctx(NULL), server(NULL), submit_q(), xio_uri(""),
   portal_id(NULL), _shutdown(false), drained(false),
   magic(0)
     {
       pthread_spin_init(&sp, PTHREAD_PROCESS_PRIVATE);
-      sq_size = 0;
 
       /* a portal is an xio_context and event loop */
       ev_loop = xio_ev_loop_create();
@@ -86,14 +148,10 @@ public:
 
   void enqueue_for_send(XioMsg *xmsg)
     {
-      pthread_spin_lock(&sp);
       if (! _shutdown) {
-	submit_q.push_back(*xmsg);
-	++sq_size;
-	if (sq_size > 0)
-	  xio_ev_loop_stop(ev_loop, false);
+	submit_q.enq(xmsg);
+	xio_ev_loop_stop(ev_loop, false);
       }
-      pthread_spin_unlock(&sp);
     }
 
   void *entry()
@@ -103,21 +161,23 @@ public:
       XioMsg::Queue::iterator q_iter;
       XioConnection *xcon;
       struct xio_msg *req;
+      uint64_t timestamp;
       XioMsg *xmsg;
 
       do {
-	/* barrier */
+	submit_q.deq(send_q);
+	size = send_q.size();
+
+	/* shutdown() barrier */
 	pthread_spin_lock(&sp);
+
 	if (_shutdown) {
 	  drained = true;
 	}
-	size = sq_size;
+
 	if (size > 0) {
-	  send_q.splice(send_q.end(), submit_q);
-	  sq_size = 0;
-	  /* submit and send may run in parallel */
-	  pthread_spin_unlock(&sp);
 	  /* XXX look out, no flow control */
+	  timestamp = 0;
 	  for (ix = 0; ix < size; ++ix) {
 	    q_iter = send_q.begin();
 	    xmsg = &(*q_iter);
@@ -127,14 +187,20 @@ public:
 	    /* handle response traffic */
 	    if (! req->request) {
 	      (void) xio_send_request(xcon->conn, req);
-	      xcon->send.set(req->timestamp);
+	      timestamp = req->timestamp;
 	    }
 	    else
 	      (void) xio_send_response(req);
 	  }
-	} else
-	  pthread_spin_unlock(&sp);
+	}
+
+	pthread_spin_unlock(&sp);
+
+	if (timestamp)
+	  xcon->send.set(timestamp); /* XXX will tolerate non-monotonic */
+
 	xio_ev_loop_run_timeout(ev_loop, 300);
+
       } while ((!_shutdown) || (!drained));
 
       /* shutting down */
