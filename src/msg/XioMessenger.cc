11121b6bc34b6cd1b894894b2353bddca3749d61
Matt Benjamin <matt@linuxbox.com>
2014-09-03 15:46:57 -0400

    Avoid deadklock, xio_disconnect can deliver a session teardown event.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/msg/XioMessenger.cc
+++ src/msg/XioMessenger.cc
@@ -801,7 +801,6 @@ assert(req->out.pdata_iov.nents || !nbuffers);
 
 int XioMessenger::shutdown()
 {
-  Mutex::Locker lck(sh_mtx);
   shutdown_called.set(true);
   conns_sp.lock();
   XioConnection::ConnList::iterator iter;
@@ -811,7 +810,9 @@ int XioMessenger::shutdown()
   }
   conns_sp.unlock();
   while(nsessions.read() > 0) {
-    sh_cond.Wait();
+    Mutex::Locker lck(sh_mtx);
+    if (nsessions.read() > 0)
+      sh_cond.Wait();
   }
   portals.shutdown();
   dispatch_strategy->shutdown();
