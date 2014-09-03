2dd9a73047e7e1bdebcac9da497a32c80a3faf0d
Matt Benjamin <matt@linuxbox.com>
2014-09-03 15:46:57 -0400

    Avoid deadlock, xio_disconnect can deliver a session teardown event.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

--- src/msg/xio/XioMessenger.cc
+++ src/msg/xio/XioMessenger.cc
@@ -814,7 +814,6 @@ assert(req->out.pdata_iov.nents || !nbuffers);
 
 int XioMessenger::shutdown()
 {
-  Mutex::Locker lck(sh_mtx);
   shutdown_called.set(true);
   conns_sp.lock();
   XioConnection::ConnList::iterator iter;
@@ -824,7 +823,9 @@ int XioMessenger::shutdown()
   }
   conns_sp.unlock();
   while(nsessions.read() > 0) {
-    sh_cond.Wait();
+    Mutex::Locker lck(sh_mtx);
+    if (nsessions.read() > 0)
+      sh_cond.Wait();
   }
   portals.shutdown();
   dispatch_strategy->shutdown();
