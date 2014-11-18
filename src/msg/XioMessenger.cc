f221685aac7d84d65e229c4de1b76235fff2c2fd
Eyal Salomon <esalomon@mellanox.com>
2014-11-18 14:05:19 -0500

    Adapt to recent changes on Accelio for_next.
    * Accelio options now of opaque type
    * RDMA behavior now governed by 2 new options
    
       IO_OPTNAME_MAX_INLINE_DATA
       XIO_OPTNAME_MAX_INLINE_HEADER
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/msg/XioMessenger.cc
+++ src/msg/XioMessenger.cc
@@ -167,6 +167,7 @@ static int on_ow_msg_send_complete(struct xio_session *session,
 
 static int on_msg_error(struct xio_session *session,
 			enum xio_status error,
+			enum xio_msg_direction dir,
 			struct xio_msg  *msg,
 			void *conn_user_context)
 {
@@ -269,34 +270,37 @@ XioMessenger::XioMessenger(CephContext *cct, entity_name_t name,
 
       xio_init();
 
-      unsigned xopt;
+      int xopt;
 
       xopt = xio_log::get_level();
       xio_set_opt(NULL, XIO_OPTLEVEL_ACCELIO, XIO_OPTNAME_LOG_LEVEL,
-		  &xopt, sizeof(unsigned));
+		  &xopt, sizeof(xopt));
       xio_set_opt(NULL, XIO_OPTLEVEL_ACCELIO, XIO_OPTNAME_LOG_FN,
 		  (const void*)xio_log::log_dout, sizeof(xio_log_fn));
 
       xopt = 1;
       xio_set_opt(NULL, XIO_OPTLEVEL_ACCELIO, XIO_OPTNAME_DISABLE_HUGETBL,
-		  &xopt, sizeof(unsigned));
+		  &xopt, sizeof(int));
 
       xopt = XIO_MSGR_IOVLEN;
       xio_set_opt(NULL, XIO_OPTLEVEL_ACCELIO, XIO_OPTNAME_MAX_IN_IOVLEN,
-		  &xopt, sizeof(unsigned));
+		  &xopt, sizeof(xopt));
       xio_set_opt(NULL, XIO_OPTLEVEL_ACCELIO, XIO_OPTNAME_MAX_OUT_IOVLEN,
-		  &xopt, sizeof(unsigned));
+		  &xopt, sizeof(xopt));
 
       xopt = cct->_conf->xio_queue_depth; // defaults to 512
-      xio_set_opt(NULL, XIO_OPTLEVEL_ACCELIO, XIO_OPTNAME_QUEUE_DEPTH,
-		  &xopt, sizeof(unsigned));
+      xio_set_opt(NULL, XIO_OPTLEVEL_ACCELIO, XIO_OPTNAME_SND_QUEUE_DEPTH,
+		  &xopt, sizeof(xopt));
+      xio_set_opt(NULL, XIO_OPTLEVEL_ACCELIO, XIO_OPTNAME_RCV_QUEUE_DEPTH,
+		  &xopt, sizeof(xopt));
 
       /* and set 0 threshold for buffer callouts */
       xopt = 0;
-      xio_set_opt(NULL, XIO_OPTLEVEL_RDMA, XIO_OPTNAME_TRANS_BUF_THRESHOLD,
-		  &xopt, sizeof(unsigned));
-      xio_set_opt(NULL, XIO_OPTLEVEL_TCP, XIO_OPTNAME_TRANS_BUF_THRESHOLD,
-		  &xopt, sizeof(unsigned));
+      xio_set_opt(NULL, XIO_OPTLEVEL_ACCELIO, XIO_OPTNAME_MAX_INLINE_DATA,
+		  &xopt, sizeof(xopt));
+      xopt = 216;
+      xio_set_opt(NULL, XIO_OPTLEVEL_ACCELIO, XIO_OPTNAME_MAX_INLINE_HEADER,
+		  &xopt, sizeof(xopt));
 
       /* unregistered pool */
 #define XMSG_MEMPOOL_QUANTUM_SMALL 4096
